17e90a6b117fa5648b7a1cb88bdb7aac49e973e7
futurebound <mitchellm1618@gmail.com>
2024-05-28 16:42:39 -0700

    style: restyled to display 4 boards for development
    wanted visual on both player self-board views and opponent
    easier to confirm DOM behavior in development

--- src/modules/GameController.js
+++ src/modules/GameController.js
@@ -4,13 +4,18 @@ import Player from './classes/Player.js';
 import Ship from './classes/Ship.js';
 
 class GameController {
-  constructor() {}
+  constructor() {
+    this.player = null;
+    this.opponent = null;
+    this.currentPlayer = null;
+  }
 
   startNewGame() {
-    const contentDiv = document.getElementById('content');
-
     const player = new Player('player1');
     const opponent = new Player('computer');
+    this.player = player;
+    this.opponent = opponent;
+    this.currentPlayer = this.player;
 
     const playerBoard = player.getBoard();
     const opponentBoard = opponent.getBoard();
@@ -38,10 +43,65 @@ class GameController {
       [3, 4],
     ]);
 
+    const contentDiv = document.getElementById('content');
+    const player1Div = document.createElement('div');
+    player1Div.classList.add('player1');
+    const player1Name = document.createElement('h1');
+    player1Name.textContent = 'player1';
+    player1Div.appendChild(player1Name);
+
+    const player2Div = document.createElement('div');
+    player2Div.classList.add('player2');
+    const player2Name = document.createElement('h1');
+    player2Name.textContent = 'player2';
+    player2Div.appendChild(player2Name);
+
     const playerBoardDiv = renderSelfBoard(playerBoard);
+    playerBoardDiv.id = 'player-self';
     const opponentBoardDiv = renderSelfBoard(opponentBoard);
-    contentDiv.appendChild(playerBoardDiv);
-    contentDiv.appendChild(opponentBoardDiv);
+    opponentBoardDiv.id = 'opponent-self';
+    player1Div.appendChild(playerBoardDiv);
+    player2Div.appendChild(opponentBoardDiv);
+
+    //testing
+    const opponentViewDiv = renderOpponentBoard(opponentBoard);
+    opponentViewDiv.id = 'opponent-board';
+    const playerViewDiv = renderOpponentBoard(playerBoard);
+    playerViewDiv.id = 'player-board';
+    player1Div.appendChild(opponentViewDiv);
+    player2Div.appendChild(playerViewDiv);
+
+    contentDiv.appendChild(player1Div);
+    contentDiv.appendChild(player2Div);
+  }
+
+  renderBoards() {
+    const contentDiv = document.getElementById('content');
+    const player1Div = document.createElement('div');
+    player1Div.classList.add('player1');
+    const player1Name = document.createElement('h1');
+    player1Name.textContent = 'player1';
+    player1Div.appendChild(player1Name);
+
+    const player2Div = document.createElement('div');
+    player2Div.classList.add('player2');
+    const player2Name = document.createElement('h1');
+    player2Name.textContent = 'player2';
+    player2Div.appendChild(player2Name);
+
+    const playerBoardDiv = renderSelfBoard(this.player.getBoard());
+    const opponentBoardDiv = renderSelfBoard(this.opponent.getBoard());
+    player1Div.appendChild(opponentBoardDiv);
+    player2Div.appendChild(playerBoardDiv);
+
+    // testing
+    const opponentViewDiv = renderOpponentBoard(opponent);
+    const playerViewDiv = renderOpponentBoard(player);
+    player1Div.appendChild(opponentViewDiv);
+    player2Div.appendChild(playerViewDiv);
+
+    contentDiv.appendChild(player1Div);
+    contentDiv.appendChild(player2Div);
   }
 
   checkEndGame() {}
